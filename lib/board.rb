e603735e0edfcd57ca9078218ad42453706e60d7
joethyr <joethayr@gmail.com>
2022-01-11 18:38:30 -0700

    updated display_board method.

--- lib/board.rb
+++ lib/board.rb
@@ -2,18 +2,20 @@ class Board
   attr_reader :coordinates
 
   def initialize
-    @coordinates = Array.new(7) { " " }
+    @coordinates = Array.new(6) { Array.new(7) { " " } }
   end
 
   def display_board
+    row_sep = "\n--------------\n"
 
-    row_sep = "\n--------------"
-    row = coordinates.map do |x|
-      "#{x}|"
+    row = coordinates.map do |i|
+      cell = i.map do |e|
+        "#{e}|"
+      end
+      cell.join('').concat(row_sep)
     end
-    row.join('').concat(row_sep)
+    row.join('')
   end
 end
-
 
 joe = Board.new
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -3,7 +3,7 @@ require 'board'
 RSpec.describe Board do
   describe 'display_board' do
     it 'displays a row of the board' do
-      expect(subject.display_board).to eql(" | | | | | | |\n--------------")
+      expect(subject.display_board).to eql(" | | | | | | |\n--------------\n | | | | | | |\n--------------\n | | | | | | |\n--------------\n | | | | | | |\n--------------\n | | | | | | |\n--------------\n | | | | | | |\n--------------\n")
     end
   end
 end
